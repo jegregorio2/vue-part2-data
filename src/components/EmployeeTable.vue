316e464abebd8f66a14d70f90759930cbd78f5f8
Juan <juan.e.gregorio@gmail.com>
2021-05-13 20:52:23 -0700

    Initial commit

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="employee-table">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- add v-for -->
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmployeeTable',
+    // add props
+    props: {
+      employees: Array,
+    },
+  }
+</script>
+
+<style scoped></style>
